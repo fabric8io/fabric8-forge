f78417791c02bf9a0f186e1b308d7711d247b777
Claus Ibsen <claus.ibsen@gmail.com>
2016-01-21 10:36:13 +0100

    Update lic headers

--- addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/CamelDeleteNodeXmlCommand.java
+++ addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/CamelDeleteNodeXmlCommand.java
@@ -1,17 +1,17 @@
 /**
- * Copyright 2005-2015 Red Hat, Inc.
- * <p/>
- * Red Hat licenses this file to you under the Apache License, version
- * 2.0 (the "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied.  See the License for the specific language governing
- * permissions and limitations under the License.
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 package io.fabric8.forge.camel.commands.project;
 
@@ -113,13 +113,14 @@ public class CamelDeleteNodeXmlCommand extends AbstractCamelProjectCommand {
 
                     // remove the lines
                     while (delta > 0) {
+                        delta--;
                         lines.remove(idx);
                     }
 
                     // and save the file back
                     String content = LineNumberHelper.linesToString(lines);
                     file.setContents(content);
-                    return Results.success("Removed node: " + nodeValue);
+                    return Results.success("Removed node");
                 }
             }
             return Results.fail("Cannot find Camel node in XML file: " + nodeValue);
