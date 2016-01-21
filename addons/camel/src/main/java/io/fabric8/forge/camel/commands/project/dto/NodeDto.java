1a9e47fb7180db40fcea094db24c503fb62b31de
Claus Ibsen <claus.ibsen@gmail.com>
2016-01-21 10:41:13 +0100

    Update lic header

--- addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/dto/NodeDto.java
+++ addons/camel/src/main/java/io/fabric8/forge/camel/commands/project/dto/NodeDto.java
@@ -1,18 +1,17 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Copyright 2005-2015 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 package io.fabric8.forge.camel.commands.project.dto;
 
@@ -22,11 +21,11 @@ import java.util.List;
 import java.util.Map;
 
 public class NodeDto extends NodeDtoSupport {
-    private List<NodeDto> children = new ArrayList<>();
-
+    private List<NodeDto> children;
     private Map<String,String> properties;
 
     public NodeDto() {
+        children = new ArrayList<>();
     }
 
     public NodeDto(String key, String id, String label, String pattern, String description, List<NodeDto> children) {
@@ -74,4 +73,5 @@ public class NodeDto extends NodeDtoSupport {
     public void setProperties(Map<String, String> properties) {
         this.properties = properties;
     }
+
 }
