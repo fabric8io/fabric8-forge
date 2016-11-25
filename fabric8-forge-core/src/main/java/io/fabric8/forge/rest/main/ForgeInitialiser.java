d0e65af2f34ae4dbc1aad5d438321ce98ac63a37
Claus Ibsen <claus.ibsen@gmail.com>
2016-11-25 18:49:38 +0100

    #601: Pre create projects on startup to be faster later

--- fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/ForgeInitialiser.java
+++ fabric8-forge-core/src/main/java/io/fabric8/forge/rest/main/ForgeInitialiser.java
@@ -17,31 +17,25 @@ package io.fabric8.forge.rest.main;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.ws.rs.core.Response;
 
 import io.fabric8.forge.rest.CommandsResource;
 import io.fabric8.forge.rest.dto.CommandInfoDTO;
-import io.fabric8.forge.rest.dto.ExecutionRequest;
-import io.fabric8.forge.rest.hooks.CommandCompletePostProcessor;
 import io.fabric8.forge.rest.producer.FurnaceProducer;
 import io.fabric8.forge.rest.utils.StopWatch;
-import io.fabric8.project.support.UserDetails;
+import org.apache.commons.io.FileUtils;
 import org.apache.deltaspike.core.api.config.ConfigProperty;
 import org.apache.maven.shared.invoker.DefaultInvocationRequest;
 import org.apache.maven.shared.invoker.DefaultInvoker;
 import org.apache.maven.shared.invoker.InvocationRequest;
 import org.apache.maven.shared.invoker.Invoker;
 import org.apache.maven.shared.invoker.MavenInvocationException;
-import org.jboss.forge.furnace.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -53,7 +47,6 @@ import org.slf4j.LoggerFactory;
 @javax.ejb.Startup
 public class ForgeInitialiser {
     private static final transient Logger LOG = LoggerFactory.getLogger(ForgeInitialiser.class);
-    public static final String DEFAULT_ARCHETYPES_VERSION = "2.2.34";
 
     /**
      * @param addOnDir the directory where Forge addons will be stored
@@ -101,7 +94,7 @@ public class ForgeInitialiser {
         LOG.info("Preloading projects");
         int i = 0;
 
-        String tempDir = createTempDirectory();
+        File tempDir = createTempDirectory();
         if (tempDir == null) {
             LOG.warn("Cannot create temporary directory");
             return;
@@ -118,7 +111,7 @@ public class ForgeInitialiser {
                 InvocationRequest request = new DefaultInvocationRequest();
                 request.setPomFile(null);
                 request.setGoals(Arrays.asList(goal));
-                request.setBaseDirectory(new File(tempDir));
+                request.setBaseDirectory(tempDir);
                 request.setInteractive(false);
                 request.setShowErrors(true);
 
@@ -133,111 +126,28 @@ public class ForgeInitialiser {
             }
         }
 
-        LOG.info("preloadProjects took " + watch.taken());
-    }
-
-    private void preloadProjectsForge(CommandsResource commandsResource, Map<String, Set<String>> catalogs)  {
-        StopWatch watch = new StopWatch();
-
-        LOG.info("Preloading projects");
-        int i = 0;
-
-        // TODO: do not work due forge classloading issues
-        // java.lang.ClassCastException: org.jboss.forge.addon.maven.archetype.ArchetypeCatalogFactory_$$_javassist_52422bd7-4060-45af-87db-4d3a6e9b5664 cannot be cast to org.jboss.forge.addon.maven.archetype.ArchetypeCatalogFactory
-
-        for (Map.Entry<String, Set<String>> entry : catalogs.entrySet()) {
-
-            String catalogName = entry.getKey();
-
-            for (String archetype : entry.getValue()) {
-
-                String tempDir = createTempDirectory();
-                if (tempDir == null) {
-                    LOG.warn("Cannot create temporary directory");
-                    continue;
-                }
-
-                i++;
-
-                ExecutionRequest executionRequest = new ExecutionRequest();
-                Map<String, Object> step1Inputs = new HashMap<>();
-                step1Inputs.put("buildSystem", "Maven");
-                String projectName = "dummy-" + i;
-                step1Inputs.put("named", projectName);
-                step1Inputs.put("targetLocation", tempDir);
-                step1Inputs.put("topLevelPackage", "org.example");
-                step1Inputs.put("type", "From Archetype Catalog");
-                step1Inputs.put("version", "1.0.0-SNAPSHOT");
-
-                Map<String, Object> step2Inputs = new HashMap<>();
-                step2Inputs.put("catalog", catalogName);
-                step2Inputs.put("archetype", archetype);
-
-                List<Map<String, Object>> inputList = new ArrayList<>();
-                inputList.add(step1Inputs);
-                inputList.add(step2Inputs);
-                executionRequest.setInputList(inputList);
-                executionRequest.setWizardStep(2);
-                UserDetails userDetails = new UserDetails("someAddress", "someInternalAddress", "dummyUser", "dummyPassword", "dummy@doesNotExist.com");
-                try {
-                    LOG.info("Now trying to create a new project: " + projectName + " from archetype: " + archetype + " in directory: " + tempDir);
-                    CommandCompletePostProcessor postProcessor = null;
-                    dumpResult(commandsResource.doExecute("project-new", executionRequest, postProcessor, userDetails, commandsResource.createUIContext(new File(tempDir))));
-
-                    LOG.info("Created project done!");
-
-                } catch (Exception e) {
-                    LOG.error("Failed to execute command: " + e, e);
-                }
-            }
+        // cleanup temporary directory
+        try {
+            FileUtils.deleteDirectory(tempDir);
+        } catch (IOException e) {
+            // ignore
         }
 
-        LOG.info("preloadProjects took " + watch.taken());
+        LOG.info("Preloading projects took " + watch.taken());
     }
 
-    protected String createTempDirectory() {
+    protected File createTempDirectory() {
         try {
-            File file = File.createTempFile("project-new", "tmp");
-            file.delete();
-            file.mkdirs();
-            return file.getPath();
+            File dir = new File("project-new");
+            // delete directory
+            FileUtils.deleteDirectory(dir);
+            dir.mkdir();
+            return dir;
         } catch (IOException e) {
             LOG.error("Failed to create temp directory: " + e, e);
         }
 
         return null;
-    }
-
-    protected void dumpResult(Response response) {
-        LOG.info("Status: "+ response.getStatus());
-        Object entity = response.getEntity();
-        if (entity != null) {
-            LOG.info("Got entity: "+ entity + " " + entity.getClass().getName());
-        }
-    }
-
-    protected String getArchetypesVersion() {
-        String answer = System.getenv("FABRIC8_ARCHETYPES_VERSION");
-        if (Strings.isNullOrEmpty(answer)) {
-            return DEFAULT_ARCHETYPES_VERSION;
-        }
-        return answer;
-    }
-
-    public static void removeDir(File d) {
-        String[] list = d.list();
-        if (list == null) {
-            list = new String[0];
-        }
-        for (String s : list) {
-            File f = new File(d, s);
-            if (f.isDirectory()) {
-                removeDir(f);
-            } else {
-                f.delete();
-            }
-        }
-        d.delete();
     }
 
 }
